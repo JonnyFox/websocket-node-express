7601aad8640256972fca1c7b65f0b850c4b405ef
jfox <fox.jonny@gmail.com>
2018-05-17 22:25:13 +0200

    fix it!

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -77,6 +77,10 @@ export class AppComponent implements AfterViewInit {
     }
 
     private getDiff(): number {
+        if (!this.viewer) {
+            return -1;
+        }
+
         const nativeElement = this.viewer.nativeElement;
         return nativeElement.scrollHeight - (nativeElement.scrollTop + nativeElement.clientHeight);
     }
